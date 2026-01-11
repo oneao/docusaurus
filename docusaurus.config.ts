103742f9c62ca14c775b5d1afa530b91c90b18bf
oneao <hi_itao@163.com>
2026-01-11 21:00:18 +0800

    u

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,8 @@ const config: Config = {
   url: 'https://www.oneao.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
